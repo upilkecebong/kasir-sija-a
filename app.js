e7cf4380a904464e7316f393322e4a2ef86c5a6e
upilkecebong <upilkecebong24@gmail.com>
2023-09-24 19:02:23 +0700

    1st commit, done front-end

---
+++ app.js
@@ -0,0 +1,26 @@
+let products = [
+    {
+        id: 0,
+        name: 'DAWET SEGER',
+        image: 'images/dawet.jpeg',
+        price: 5000
+    },
+    {
+        id: 1,
+        name: 'CENIL MUNGIL',
+        image: 'images/cenil.jpeg',
+        price: 2000
+    },
+    {
+        id: 2,
+        name: 'BAKWAN TELO',
+        image: 'images/bakwan.jfif',
+        price: 2000
+    },
+    {
+        id: 3,
+        name: 'WAJIK KETAN',
+        image: 'images/wajik.jpg',
+        price: 2000
+    },
+]
