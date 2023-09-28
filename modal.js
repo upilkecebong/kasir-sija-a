85f7b02058698ccbe5f998287ef043ade9a82b24
upilkecebong <upilkecebong24@gmail.com>
2023-09-28 21:23:07 +0700

    midal

---
+++ modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var a = document.getElementsByClassName("agree")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+a.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
